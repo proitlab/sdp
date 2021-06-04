952daaeb2ebcdd9daaa70d30db2048fb6c56d7a9
dedysutanto <dedysutanto@gmail.com>
2021-06-04 10:59:25 +0700

    List Display

--- data/admin.py
+++ data/admin.py
@@ -4,7 +4,8 @@ from data.models import *
 # Register your models here.
 
 class ItemsAdmin(admin.ModelAdmin):
-    list_filter = ('condition',)
+    list_filter = ('condition','serialnumber',)
+    list_display = ('item_detail','serialnumber','vendor',)
     
 
 
